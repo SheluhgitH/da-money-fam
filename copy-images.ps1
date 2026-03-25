f5c5b871d90b1c815782ec7dc669a57fe27506c3
sheluhgitH <leadalwayz@gmail.com>
2026-03-25 16:53:24 -0400

    chore: restore da-money-fam subdirectory for Vercel deployment compliance

--- copy-images.ps1
+++ copy-images.ps1
@@ -1,26 +1,22 @@
-# Copy artist images script
-# Run this by right-clicking and selecting "Run with PowerShell"
-
-$sourcePath = "C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics"
-$destPath = "C:\Users\Pharp\Desktop\DMF APPS\Site 2\da-money-fam\public\images"
+$dest = 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\da-money-fam\public\images'
 
 # Jackpot Pics
-Copy-Item "$sourcePath\Jackpot Pics\IMG_1223.png" "$destPath\"
-Copy-Item "$sourcePath\Jackpot Pics\IMG_1222.png" "$destPath\"
-Copy-Item "$sourcePath\Jackpot Pics\Jackpotpic4.PNG" "$destPath\jackpot-extra-1.PNG"
-Copy-Item "$sourcePath\Jackpot Pics\JacpotMagazine1.jpg" "$destPath\jackpot-magazine-1.jpg"
-Copy-Item "$sourcePath\Jackpot Pics\JackpotMagazine2.jpg" "$destPath\jackpot-magazine-2.jpg"
-Copy-Item "$sourcePath\Jackpot Pics\jackpotpic2.PNG" "$destPath\jackpot-extra-2.PNG"
-Copy-Item "$sourcePath\Jackpot Pics\jackpotpic3.PNG" "$destPath\jackpot-extra-3.PNG"
-Copy-Item "$sourcePath\Jackpot Pics\jackpotpic1.PNG" "$destPath\jackpot-extra-4.PNG"
+Copy-Item 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\IMG_1223.png' -Destination "$dest\IMG_1223.png" -Force
+Copy-Item 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\IMG_1222.png' -Destination "$dest\IMG_1222.png" -Force
+Copy-Item 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\Jackpotpic4.PNG' -Destination "$dest\jackpot-extra-1.PNG" -Force
+Copy-Item 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\JacpotMagazine1.jpg' -Destination "$dest\jackpot-magazine-1.jpg" -Force
+Copy-Item 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\JackpotMagazine2.jpg' -Destination "$dest\jackpot-magazine-2.jpg" -Force
+Copy-Item 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\jackpotpic2.PNG' -Destination "$dest\jackpot-extra-2.PNG" -Force
+Copy-Item 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\jackpotpic3.PNG' -Destination "$dest\jackpot-extra-3.PNG" -Force
+Copy-Item 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\jackpotpic1.PNG' -Destination "$dest\jackpot-extra-4.PNG" -Force
 
 # Vlone Tr3 Pics
-Copy-Item "$sourcePath\Vlone Tr3 Pics\IMG_1220.png" "$destPath\vlonetr3-1.png"
-Copy-Item "$sourcePath\Vlone Tr3 Pics\IMG_1219.png" "$destPath\vlonetr3-2.png"
+Copy-Item 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Vlone Tr3 Pics\IMG_1220.png' -Destination "$dest\vlonetr3-1.png" -Force
+Copy-Item 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Vlone Tr3 Pics\IMG_1219.png' -Destination "$dest\vlonetr3-2.png" -Force
 
 # SideShowDaPlug Pics
-Copy-Item "$sourcePath\SideShowDaPlug Pics\IMG_1228.png" "$destPath\sideshowdaplug-1.png"
-Copy-Item "$sourcePath\SideShowDaPlug Pics\IMG_1229.png" "$destPath\sideshowdaplug-2.png"
-Copy-Item "$sourcePath\SideShowDaPlug Pics\IMG_1221.png" "$destPath\sideshowdaplug-3.png"
+Copy-Item 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\SideShowDaPlug Pics\IMG_1228.png' -Destination "$dest\sideshowdaplug-1.png" -Force
+Copy-Item 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\SideShowDaPlug Pics\IMG_1229.png' -Destination "$dest\sideshowdaplug-2.png" -Force
+Copy-Item 'C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\SideShowDaPlug Pics\IMG_1221.png' -Destination "$dest\sideshowdaplug-3.png" -Force
 
-Write-Host "All images copied successfully!"
+Write-Host 'All files copied successfully!' -ForegroundColor Green
---
+++ copy_jackpot.bat
@@ -0,0 +1,9 @@
+@echo off
+mkdir "C:\Users\Pharp\Desktop\DMF APPS\Site 2\da-money-fam\public\images\jackpot-gallery" 2>nul
+copy "C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\Jackpotpic4.PNG" "C:\Users\Pharp\Desktop\DMF APPS\Site 2\da-money-fam\public\images\jackpot-gallery\"
+copy "C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\JacpotMagazine1.jpg" "C:\Users\Pharp\Desktop\DMF APPS\Site 2\da-money-fam\public\images\jackpot-gallery\"
+copy "C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\JackpotMagazine2.jpg" "C:\Users\Pharp\Desktop\DMF APPS\Site 2\da-money-fam\public\images\jackpot-gallery\"
+copy "C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\jackpotpic2.PNG" "C:\Users\Pharp\Desktop\DMF APPS\Site 2\da-money-fam\public\images\jackpot-gallery\"
+copy "C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\jackpotpic3.PNG" "C:\Users\Pharp\Desktop\DMF APPS\Site 2\da-money-fam\public\images\jackpot-gallery\"
+copy "C:\Users\Pharp\Desktop\DMF APPS\Site 2\Dmf site pics\Jackpot Pics\jackpotpic1.PNG" "C:\Users\Pharp\Desktop\DMF APPS\Site 2\da-money-fam\public\images\jackpot-gallery\"
+echo Done!
